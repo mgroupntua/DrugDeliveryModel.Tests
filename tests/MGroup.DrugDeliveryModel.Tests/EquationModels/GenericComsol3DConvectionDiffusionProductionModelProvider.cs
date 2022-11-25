3c8ac065c01bcc3d8f9e202f404e2e4afe24bec9
Gerasimos <acivil@hotmail.gr>
2022-11-25 12:45:07 +0200

    added equation 1 example initial conditions.

--- tests/MGroup.DrugDeliveryModel.Tests/EquationModels/GenericComsol3DConvectionDiffusionProductionModelProvider.cs
+++ tests/MGroup.DrugDeliveryModel.Tests/EquationModels/GenericComsol3DConvectionDiffusionProductionModelProvider.cs
@@ -62,27 +62,41 @@ namespace MGroup.DrugDeliveryModel.Tests.EquationModels
                 model.SubdomainsDictionary[0].Elements.Add(element);
             }
 
+            
+            return model;
+        }
+
+
+        /// <summary>
+        /// gia to model workingtetmesh155 to 0.1, 0.1, 0.,1 kuvo 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="maxZ">value =2</param>
+        /// <param name="topValueprescribed"></param>
+        /// <param name="minZ">value = 0</param>
+        /// <param name="bottomValueprescribed"></param>
+        /// <returns></returns>
+        public void AddTopAndBottomBCs(Model model, double modelMaxZ, double topValueprescribed, double modelMinZ, double bottomValueprescribed)
+        {
+            
             var topNodes = new List<INode>();
             var bottomNodes = new List<INode>();
             var innerBulkNodes = new List<INode>();
             foreach (var node in model.NodesDictionary.Values)
             {
-                if (Math.Abs(2 - node.Z) < 1E-9) topNodes.Add(node);
-                else if (Math.Abs(0 - node.Z) < 1E-9) bottomNodes.Add(node);
+                if (Math.Abs(modelMaxZ - node.Z) < 1E-9) topNodes.Add(node);
+                else if (Math.Abs(modelMinZ - node.Z) < 1E-9) bottomNodes.Add(node);
                 else innerBulkNodes.Add(node);
             }
 
-            int i = 0;
-            var dirichletBCs = new NodalUnknownVariable[topNodes.Count + bottomNodes.Count];
+            var dirichletBCs = new List<NodalUnknownVariable>();
             foreach (var node in topNodes)
             {
-                dirichletBCs[i] = new NodalUnknownVariable(node, ConvectionDiffusionDof.UnknownVariable, 100d);
-                i++;
+                dirichletBCs.Add(new NodalUnknownVariable(node, ConvectionDiffusionDof.UnknownVariable, topValueprescribed));            
             }
             foreach (var node in bottomNodes)
             {
-                dirichletBCs[i] = new NodalUnknownVariable(node, ConvectionDiffusionDof.UnknownVariable, 50d);
-                i++;
+                dirichletBCs.Add(new NodalUnknownVariable(node, ConvectionDiffusionDof.UnknownVariable, bottomValueprescribed));
             }
 
             model.BoundaryConditions.Add(new ConvectionDiffusionBoundaryConditionSet(
@@ -90,121 +104,39 @@ namespace MGroup.DrugDeliveryModel.Tests.EquationModels
                 new INodalConvectionDiffusionNeumannBoundaryCondition[] { }
             ));
 
-            var intitalConditions = new List<INodalConvectionDiffusionInitialCondition>();
+        }
+
+        public void AddInitialConditionsForTheRestOfBulkNodes(Model model, double modelMaxZ, double modelMinZ, double prescribedInitialConditionValueForBulk)
+        {
+
+            var topNodes = new List<INode>();
+            var bottomNodes = new List<INode>();
+            var innerBulkNodes = new List<INode>();
             foreach (var node in model.NodesDictionary.Values)
             {
-                if ((!topNodes.Contains(node))&& (!bottomNodes.Contains(node)))
-                {
-                    intitalConditions.Add(new NodalInitialUnknownVariable(node, ConvectionDiffusionDof.UnknownVariable, initialConditionAmount);
-                }
-                
+                if (Math.Abs(modelMaxZ - node.Z) < 1E-9) topNodes.Add(node);
+                else if (Math.Abs(modelMinZ - node.Z) < 1E-9) bottomNodes.Add(node);
+                else innerBulkNodes.Add(node);
             }
+
+            var intitalConditions = new List<INodalConvectionDiffusionInitialCondition>();
+            foreach (var node in innerBulkNodes)
+            {
+                if ((!topNodes.Contains(node)) && (!bottomNodes.Contains(node)))
+                {
+                    intitalConditions.Add(new NodalInitialUnknownVariable(node, ConvectionDiffusionDof.UnknownVariable, prescribedInitialConditionValueForBulk));
+                }
+
+            }
+
+
+            model.InitialConditions.Add(new ConvectionDiffusionInitialConditionSet(intitalConditions,
+                new DomainInitialUnknownVariable[]
+                { }));
 
             
-            //model.InitialConditions.Add(new StructuralInitialConditionSet(
-            //    new[]
-            //    {
-            //        new NodalInitialDisplacement(model.NodesDictionary[1], StructuralDof.TranslationX, 0),
-            //    },
-            //    new[]
-            //    {
-            //        new DomainInitialVelocity(StructuralDof.TranslationZ, 1),
-            //    }));
-
-            return model;
         }
 
-        public Model CreateModel()
-        {
-            var model = new Model();
-            model.SubdomainsDictionary[0] = new Subdomain(id: 0);
-
-            int nodeIndex = -1;
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 2.0, y: 2.0, z: 2.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 2.0, y: 1.0, z: 2.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 2.0, y: 0.0, z: 2.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 2.0, y: 2.0, z: 1.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 2.0, y: 1.0, z: 1.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 2.0, y: 0.0, z: 1.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 2.0, y: 2.0, z: 0.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 2.0, y: 1.0, z: 0.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 2.0, y: 0.0, z: 0.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 1.0, y: 2.0, z: 2.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 1.0, y: 1.0, z: 2.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 1.0, y: 0.0, z: 2.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 1.0, y: 2.0, z: 1.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 1.0, y: 1.0, z: 1.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 1.0, y: 0.0, z: 1.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 1.0, y: 2.0, z: 0.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 1.0, y: 1.0, z: 0.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 1.0, y: 0.0, z: 0.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 0.0, y: 2.0, z: 2.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 0.0, y: 1.0, z: 2.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 0.0, y: 0.0, z: 2.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 0.0, y: 2.0, z: 1.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 0.0, y: 1.0, z: 1.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 0.0, y: 0.0, z: 1.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 0.0, y: 2.0, z: 0.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 0.0, y: 1.0, z: 0.0);
-            model.NodesDictionary[++nodeIndex] = new Node(id: nodeIndex, x: 0.0, y: 0.0, z: 0.0);
-
-            var elementNodes = new INode[][]
-            {
-                new[] { model.NodesDictionary[13], model.NodesDictionary[4], model.NodesDictionary[3], model.NodesDictionary[12], model.NodesDictionary[10], model.NodesDictionary[1], model.NodesDictionary[0], model.NodesDictionary[9] },
-                new[] { model.NodesDictionary[14], model.NodesDictionary[5], model.NodesDictionary[4], model.NodesDictionary[13], model.NodesDictionary[11], model.NodesDictionary[2], model.NodesDictionary[1], model.NodesDictionary[10] },
-                new[] { model.NodesDictionary[16], model.NodesDictionary[7], model.NodesDictionary[6], model.NodesDictionary[15], model.NodesDictionary[13], model.NodesDictionary[4], model.NodesDictionary[3], model.NodesDictionary[12] },
-                new[] { model.NodesDictionary[17], model.NodesDictionary[8], model.NodesDictionary[7], model.NodesDictionary[16], model.NodesDictionary[14], model.NodesDictionary[5], model.NodesDictionary[4], model.NodesDictionary[13] },
-                new[] { model.NodesDictionary[22], model.NodesDictionary[13], model.NodesDictionary[12], model.NodesDictionary[21], model.NodesDictionary[19], model.NodesDictionary[10], model.NodesDictionary[9], model.NodesDictionary[18] },
-                new[] { model.NodesDictionary[23], model.NodesDictionary[14], model.NodesDictionary[13], model.NodesDictionary[22], model.NodesDictionary[20], model.NodesDictionary[11], model.NodesDictionary[10], model.NodesDictionary[19] },
-                new[] { model.NodesDictionary[25], model.NodesDictionary[16], model.NodesDictionary[15], model.NodesDictionary[24], model.NodesDictionary[22], model.NodesDictionary[13], model.NodesDictionary[12], model.NodesDictionary[21] },
-                new[] { model.NodesDictionary[26], model.NodesDictionary[17], model.NodesDictionary[16], model.NodesDictionary[25], model.NodesDictionary[23], model.NodesDictionary[14], model.NodesDictionary[13], model.NodesDictionary[22] },
-            };
-
-            var nodeReordering = new GMeshElementLocalNodeOrdering();
-            var rearrangeNodes = elementNodes.Select(x => nodeReordering.ReorderNodes(x, CellType.Hexa8)).ToArray();
-
-            var material = new ConvectionDiffusionProperties(
-                capacityCoeff: 0d,
-                diffusionCoeff: DiffusionCoeff,
-                convectionCoeff: ConvectionCoeff,
-                dependentSourceCoeff: DependentProductionCoeff,
-                independentSourceCoeff: IndependentProductionCoeff);
-
-            var elementFactory = new ConvectionDiffusionElement3DFactory(material);
-            for (int i = 0; i < elementNodes.Length; i++)
-            {
-                model.ElementsDictionary[i] = elementFactory.CreateElement(CellType.Hexa8, rearrangeNodes[i]);
-                model.ElementsDictionary[i].ID = i;
-                model.SubdomainsDictionary[0].Elements.Add(model.ElementsDictionary[i]);
-            }
-;
-            model.BoundaryConditions.Add(new ConvectionDiffusionBoundaryConditionSet(
-                new[]
-                {
-                    new NodalUnknownVariable(model.NodesDictionary[0], ConvectionDiffusionDof.UnknownVariable,  100d),
-                    new NodalUnknownVariable(model.NodesDictionary[1], ConvectionDiffusionDof.UnknownVariable,  100d),
-                    new NodalUnknownVariable(model.NodesDictionary[2], ConvectionDiffusionDof.UnknownVariable,  100d),
-                    new NodalUnknownVariable(model.NodesDictionary[9], ConvectionDiffusionDof.UnknownVariable,  100d),
-                    new NodalUnknownVariable(model.NodesDictionary[10], ConvectionDiffusionDof.UnknownVariable, 100d),
-                    new NodalUnknownVariable(model.NodesDictionary[11], ConvectionDiffusionDof.UnknownVariable, 100d),
-                    new NodalUnknownVariable(model.NodesDictionary[18], ConvectionDiffusionDof.UnknownVariable, 100d),
-                    new NodalUnknownVariable(model.NodesDictionary[19], ConvectionDiffusionDof.UnknownVariable, 100d),
-                    new NodalUnknownVariable(model.NodesDictionary[20], ConvectionDiffusionDof.UnknownVariable, 100d),
-                    new NodalUnknownVariable(model.NodesDictionary[6], ConvectionDiffusionDof.UnknownVariable,  50d),
-                    new NodalUnknownVariable(model.NodesDictionary[7], ConvectionDiffusionDof.UnknownVariable,  50d),
-                    new NodalUnknownVariable(model.NodesDictionary[8], ConvectionDiffusionDof.UnknownVariable,  50d),
-                    new NodalUnknownVariable(model.NodesDictionary[15], ConvectionDiffusionDof.UnknownVariable, 50d),
-                    new NodalUnknownVariable(model.NodesDictionary[16], ConvectionDiffusionDof.UnknownVariable, 50d),
-                    new NodalUnknownVariable(model.NodesDictionary[17], ConvectionDiffusionDof.UnknownVariable, 50d),
-                    new NodalUnknownVariable(model.NodesDictionary[24], ConvectionDiffusionDof.UnknownVariable, 50d),
-                    new NodalUnknownVariable(model.NodesDictionary[25], ConvectionDiffusionDof.UnknownVariable, 50d),
-                    new NodalUnknownVariable(model.NodesDictionary[26], ConvectionDiffusionDof.UnknownVariable, 50d),
-                },
-                new INodalConvectionDiffusionNeumannBoundaryCondition[] { }
-            ));
-
-            return model;
-        }
 
 
     }
